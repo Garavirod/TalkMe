1ca718cf14a039de113ff61cd92b75c0cb9f6f73
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-11-22 20:46:51 -0600

    API unplash consumed on description pictures

--- src/views/DescribePic.vue
+++ src/views/DescribePic.vue
@@ -7,23 +7,55 @@
                     <v-card class="pa-2">
                         <!--CONTROLS-->
                         <v-row class="text-center">
-                            <v-col cols="12" lg="4">
-                                <v-btn small outlined color="#3b6978">Change picture</v-btn>
+                           
+                            <!--FILTERS-->
+                            <v-col cols="12" lg="6">
+                                <v-row>
+                                    <v-col cols="12" lg="12">
+                                        <v-text-field
+                                            v-model="kwordFilter"
+                                            label="Filter pictures by kword"
+                                            outlined                                            
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" lg="12">
+                                        <v-btn 
+                                            small 
+                                            outlined 
+                                            color="#3b6978"
+                                            :disabled="validFilterField"
+                                            @click="getPictures"
+                                        >
+                                            Filter
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>                                 
                             </v-col>
-                            <v-col cols="12" lg="4">
-                                <FliterPicture />
-                            </v-col>
-                            <v-col cols="12" lg="4">
-                                <CluesPicture />
-                            </v-col>
+                             <v-col cols="12" lg="6">
+                                <v-row>
+                                    <v-col cols="12" lg="12">
+                                        <CluesPicture />
+                                    </v-col>
+                                    <v-col cols="12" lg="12">
+                                        <v-btn 
+                                            small 
+                                            outlined 
+                                            color="#3b6978"
+                                            @click="changeNextPicture"
+                                        >
+                                            Change picture
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>                                
+                            </v-col>                            
                         </v-row>
                         <!--PICTURE-->
                         <v-row class="text-center">
                             <v-col cols="12" lg="9" md="12" xs="12" sm="12" class="mx-auto">
                                 <v-img :src="current_image"  aspect-ratio="1" class="grey lighten-2" max-height="500">
-                                    <template v-slot:placeholder>
+                                    <template>
                                         <v-row class="fill-height ma-0" align="center" justify="center">
-                                            <v-progress-circular indeterminate color="blue"></v-progress-circular>
+                                            <v-progress-circular indeterminate color="blue" v-if="preload"></v-progress-circular>
                                         </v-row>
                                     </template>
                                 </v-img>
@@ -45,36 +77,57 @@
 import SectionCard from "../components/SectionCard";
 import AudioControls from "../components/AudioControls";
 import CluesPicture from "../components/CluesPicture";
-import FliterPicture from "../components/FiltersPicture";
+// import FliterPicture from "../components/FiltersPicture";
 import axios from "axios";
 export default {
-    name: "DescribePic",
-    current_image:"",
+    name: "DescribePic",    
     components: {
         SectionCard,
         AudioControls,
         CluesPicture,
-        FliterPicture
+        // FliterPicture
     },
     data: () => ({
+        preload:true,
+        current_image:"",
+        kwordFilter:"Animals",
+        picturesResults:[],
+        idxPictArray:0,
         sectionName: "Describing pictures",
         extract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas tempora, quia consequatur sequi rem quidem maiores adipisci, quis iusto veniam rerum, placeat dolore delectus earum ratione. Inventore ullam veritatis quae.",
         instructions: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas tempora, quia consequatur sequi rem quidem maiores adipisci, quis iusto veniam rerum, placeat dolore delectus earum ratione. Inventore ullam veritatis quae.",
     }),
+    computed:{
+        validFilterField(){
+            return (this.kwordFilter==="") ? true:false;
+        }
+    },
     methods:{        
         async getPictures(){
-            const key = "g9RhX8mCpp5TjdicEI4UCpckeh5zPXl7k3dckGbeE34";
-            const keyword = "people";
-            const limit = 10;
-            const url = `https://api.unsplash.com/search/photos?page=${limit}&query=${keyword}&client_id=${key}`;            
+            this.current_image="";
+            this.preload=true;
+            const key = "g9RhX8mCpp5TjdicEI4UCpckeh5zPXl7k3dckGbeE34";            
+            const limit = 12;
+            const url = `https://api.unsplash.com/search/photos?per_page=${limit}&query=${this.kwordFilter}&client_id=${key}`;            
             await axios.get(url)
             .then((data)=>{
-                this.current_image=data.data.results[0].urls.thumb;
-                console.log(this.current_image);
+                this.picturesResults = data.data.results; 
+                this.current_image= this.picturesResults[this.idxPictArray].urls.full;
+                this.preload=false;
+
+                // console.log(this.picturesResults);
             })
             .catch(err=>{
                 console.log("Error on pettition",err);
             });
+        },
+        changeNextPicture(){
+            if(this.idxPictArray<this.picturesResults.length-1){
+                this.idxPictArray +=1;
+            }else{
+                this.idxPictArray = 0;                
+            }
+            this.current_image= this.picturesResults[this.idxPictArray].urls.full;
         }
     },
     created() {
