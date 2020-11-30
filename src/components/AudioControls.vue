010f5dc9a46fcd724dd82e9325311d9213cab98b
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-11-29 20:10:08 -0600

    Audio recoreder functionality was added

--- src/components/AudioControls.vue
+++ src/components/AudioControls.vue
@@ -2,19 +2,50 @@
 <!--BUTTONS CONTROLS-->
 <v-row class="text-center ma-2" id="AudioComponent">
     <v-col cols="12">
+         <v-select 
+          v-model="itemDevice"          
+          :items="listDevices"     
+          :hint="`${itemDevice.label} ${itemDevice.devId}`"     
+          item-text="label"
+          item-value="devId"
+          persistent-hint
+          return-object
+          single-line
+          label="Choose an audio devices">
+        </v-select>
+    </v-col>
+    <v-col cols="12">
         <v-btn small class="mx-2" fab dark color="#3b6978">
             <v-icon dark>mdi-send</v-icon>
         </v-btn>
-        <v-btn small class="mx-2" fab dark color="red" @click="isrecording = !isrecording">
-            <v-icon dark v-if="isrecording">mdi-stop</v-icon>
-            <v-icon dark v-else>mdi-microphone</v-icon>
+        <v-btn 
+            small 
+            class="mx-2" 
+            fab 
+            dark 
+            color="red" 
+            @click="startRecording"
+            v-if="!isrecording"
+            >
+            <v-icon dark >mdi-microphone</v-icon>
+        </v-btn>
+        <v-btn 
+            small 
+            class="mx-2" 
+            fab 
+            dark 
+            color="red" 
+            @click="stopRecording"
+            v-if="isrecording"
+            >
+            <v-icon dark>mdi-stop</v-icon>
         </v-btn>
     </v-col>
     <v-col v-if="isrecording">
         <v-row class="text-center outlined">
-            <v-col cols="12" lg="4" xs="4" sm="4">00</v-col>
-            <v-col cols="12" lg="4" xs="4" sm="4">00</v-col>
-            <v-col cols="12" lg="4" xs="4" sm="4">00</v-col>
+            <v-col cols="12" lg="4" xs="4" sm="4">{{hrs}}</v-col>
+            <v-col cols="12" lg="4" xs="4" sm="4">{{min}}</v-col>
+            <v-col cols="12" lg="4" xs="4" sm="4">{{sec}}</v-col>
         </v-row>
     </v-col>
 </v-row>
@@ -33,7 +64,137 @@ export default {
     data() {
         return {
             isrecording: false,
+            itemDevice: { label: '', devId: '' },
+            listDevices:[],
+            mediaRecorder:null,
+            audioFragments:[],
+            hrs:0,
+            min:0,
+            sec:0,
+            initTime:null,
+            idInterval:null
         }
     },
+    methods:{
+        /* 
+            This method excecutes mediaRecording start
+            for recording audio.
+         */
+        startRecording(){            
+            if(!this.listDevices.length){
+                alert("There is not devices!");            
+            }else{
+                this.isrecording = !this.isrecording;
+                navigator.mediaDevices.getUserMedia(
+                    {
+                        audio:{
+                            deviceId:this.itemDevice.devId
+                        }
+                    }
+                ).then(stream=>{
+                    this.mediaRecorder = new MediaRecorder(stream);
+                    this.mediaRecorder.start();
+                    this.startCounting();
+                    this.mediaRecorder.addEventListener("dataavailable", (e) => {                        
+                        this.audioFragments.push(e.data);
+                    });
+
+                    this.mediaRecorder.addEventListener("stop", () => {                            
+                            // Detener el stream
+                            stream.getTracks().forEach(track => track.stop());
+                            // Detener la cuenta regresiva
+                            this.stopCounting();
+                            // Convertir los fragmentos a un objeto binario
+                            const blobAudio = new Blob(this.audioFragments);
+                            this.audioFragments=[];
+                            // Crear una URL o enlace para descargar
+                            const urlParaDescargar = URL.createObjectURL(blobAudio);
+                            // Crear un elemento <a> invisible para descargar el audio
+                            let a = document.createElement("a");
+                            document.body.appendChild(a);
+                            a.style = "display: none";
+                            a.href = urlParaDescargar;
+                            a.download = "grabacion_blumin.me.webm";
+                            // Hacer click en el enlace
+                            a.click();
+                            // Y remover el objeto
+                            window.URL.revokeObjectURL(urlParaDescargar);
+                            this.isrecording = !this.isrecording;
+                    });
+                })
+                .catch(err=>{
+                    console.log('Error on recording >: ',err);
+                })               
+            }
+
+        },
+        /* 
+            This method execute the stop mediaRecording event
+            for stop recording audio
+         */
+        stopRecording(){
+            this.mediaRecorder.stop();
+            this.mediaRecorder = null;
+        },
+        /* This method clears the List of audio devices */
+        cleanListDevices(){
+            this.listDevices = [];
+        },
+        /*
+            This method converts each numSec interval
+            in hours, minutes or seconds
+        */
+        secToParseTime(numSec){
+            this.hrs= Math.floor(numSec / 3600);
+            let res = Math.floor(numSec % 3600);
+            this.min = Math.floor(res / 60);
+            this.sec = Math.floor(res % 60);
+        },
+        /*
+            This method initializes the interval recording time 
+        */
+        startCounting(){
+            this.initTime = Date.now();
+            this.idInterval = setInterval(()=>{
+                this.secToParseTime((Date.now() - this.initTime) / 1000);
+            },500);
+        },
+        /* 
+            This method clears and reset interval recording time
+        */
+        stopCounting(){
+            clearInterval(this.idInterval);
+            this.initTime = null;
+            this.hrs = 0;
+            this.min = 0;
+            this.sec = 0;
+        }
+    },
+    created(){
+
+        const hasSupport = !!(navigator.mediaDevices.getUserMedia);
+        /* 
+            When component were created this block verifies
+            if the webborwoser suppotrs MediaRecorder.
+         */
+        if(typeof MediaRecorder === "undefined" || !hasSupport){
+            alert("Your borwser not support this tachnology, please get upload it");
+        }else{
+            navigator.mediaDevices.enumerateDevices()
+            .then(devices=>{
+                this.cleanListDevices();
+                devices.forEach((div,idx)=>{
+                    if(div.kind==="audioinput"){
+                        const obj = {
+                            label : div.label || `Device ${idx + 1}`,
+                            devId : div.deviceId                        
+                        }
+                        this.listDevices.push(obj);
+                    }
+                })
+            })
+        }
+
+    }
 }
 </script>
